ebcc20a0e4f4c59af5888608c779acce7337d130
Guilherme Gaspar <guilhermegasparperroni97@gmail.com>
2020-07-25 21:09:30 -0300

    Text component
    Added a serialize field to the Story Text

---
+++ Assets/FantasyText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FantasyText : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start() {
+        textComponent.text = ("The code is working");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
